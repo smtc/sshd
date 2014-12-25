5f16bb93a10893c7ab2c191af15a42821cf2adf3
guotie <guotie.9@gmail.com>
2014-12-25 13:28:16 +0800

    add sshd.go

---
+++ sshd.go
@@ -0,0 +1,211 @@
+package main
+
+// https://gist.github.com/jpillora/b480fde82bff51a06238
+
+// A simple SSH server providing bash sessions
+//
+// Server:
+// cd my/new/dir/
+// #generate server keypair
+// ssh-keygen -t rsa
+// go get -v .
+// go run sshd.go
+//
+// Client:
+// ssh foo@localhost -p 2022 #pass=bar
+
+import (
+	"encoding/binary"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"net"
+	"os/exec"
+	"sync"
+	"syscall"
+	"unsafe"
+
+	"github.com/kr/pty"
+	"golang.org/x/crypto/ssh"
+)
+
+func main() {
+
+	// In the latest version of crypto/ssh (after Go 1.3), the SSH server type has been removed
+	// in favour of an SSH connection type. A ssh.ServerConn is created by passing an existing
+	// net.Conn and a ssh.ServerConfig to ssh.NewServerConn, in effect, upgrading the net.Conn
+	// into an ssh.ServerConn
+
+	config := &ssh.ServerConfig{
+		//Define a function to run when a client attempts a password login
+		PasswordCallback: func(c ssh.ConnMetadata, pass []byte) (*ssh.Permissions, error) {
+			// Should use constant-time compare (or better, salt+hash) in a production setting.
+			if c.User() == "foo" && string(pass) == "bar" {
+				return nil, nil
+			}
+			return nil, fmt.Errorf("password rejected for %q", c.User())
+		},
+		// You may also explicitly allow anonymous client authentication
+		// NoClientAuth: true,
+	}
+
+	// You can generate a keypair with 'ssh-keygen -t rsa'
+	privateBytes, err := ioutil.ReadFile("id_rsa")
+	if err != nil {
+		log.Fatal("Failed to load private key (./id_rsa)")
+	}
+
+	private, err := ssh.ParsePrivateKey(privateBytes)
+	if err != nil {
+		log.Fatal("Failed to parse private key")
+	}
+
+	config.AddHostKey(private)
+
+	// Once a ServerConfig has been configured, connections can be accepted.
+	listener, err := net.Listen("tcp", "0.0.0.0:2022")
+	if err != nil {
+		log.Fatal("Failed to listen on 2022")
+	}
+
+	// Accept all connections
+	log.Print("Listening on 2022...")
+	for {
+		tcpConn, err := listener.Accept()
+		if err != nil {
+			log.Printf("Failed to accept incoming connection (%s)", err)
+			continue
+		}
+		// Before use, a handshake must be performed on the incoming net.Conn.
+		sshConn, chans, reqs, err := ssh.NewServerConn(tcpConn, config)
+		if err != nil {
+			log.Printf("Failed to handshake (%s)", err)
+			continue
+		}
+
+		log.Printf("New SSH connection from %s (%s)", sshConn.RemoteAddr(), sshConn.ClientVersion())
+		// Discard all global out-of-band Requests
+		go handleRequests(reqs)
+		// Accept all channels
+		go handleChannels(chans)
+	}
+}
+
+func handleRequests(reqs <-chan *ssh.Request) {
+	for req := range reqs {
+		log.Printf("recieved out-of-band request: %+v", req)
+	}
+}
+
+func handleChannels(chans <-chan ssh.NewChannel) {
+	// Service the incoming Channel channel.
+	for newChannel := range chans {
+		// Since we're handling the execution of a shell, we expect a
+		// channel type of "session". However, there are also: "x11", "direct-tcpip"
+		// and "forwarded-tcpip" channel types.
+		if t := newChannel.ChannelType(); t != "session" {
+			newChannel.Reject(ssh.UnknownChannelType, fmt.Sprintf("unknown channel type: %s", t))
+			continue
+		}
+
+		// At this point, we have the opportunity to reject the client's
+		// request for another logical connection
+		channel, requests, err := newChannel.Accept()
+		if err != nil {
+			log.Printf("could not accept channel (%s)", err)
+			continue
+		}
+		// fire up bash for this session
+		bash := exec.Command("bash")
+		// allocate a terminal for this channel
+		log.Print("creating pty...")
+		bashf, err := pty.Start(bash)
+		if err != nil {
+			log.Printf("could not start pty (%s)", err)
+			continue
+		}
+
+		//teardown session
+		var once sync.Once
+		close := func() {
+			channel.Close()
+			_, err := bash.Process.Wait()
+			if err != nil {
+				log.Printf("failed to exit bash (%s)", err)
+			}
+			log.Printf("session closed")
+		}
+
+		//pipe session to bash and visa-versa
+		go func() {
+			io.Copy(channel, bashf)
+			once.Do(close)
+		}()
+		go func() {
+			io.Copy(bashf, channel)
+			once.Do(close)
+		}()
+
+		// Sessions have out-of-band requests such as "shell", "pty-req" and "env"
+		go func(in <-chan *ssh.Request) {
+			for req := range in {
+				ok := false
+				switch req.Type {
+				case "shell":
+					// We don't accept any commands (Payload),
+					// only the default shell.
+					if len(req.Payload) == 0 {
+						ok = true
+					}
+				case "pty-req":
+					// Responding 'ok' here will let the client
+					// know we have a pty ready for input
+					ok = true
+					// Parse body...
+					termLen := req.Payload[3]
+					termEnv := string(req.Payload[4 : termLen+4])
+					w, h := parseDims(req.Payload[termLen+4:])
+					SetWinsize(bashf.Fd(), w, h)
+					log.Printf("pty-req '%s'", termEnv)
+				case "window-change":
+					w, h := parseDims(req.Payload)
+					SetWinsize(bashf.Fd(), w, h)
+					continue //no response
+				}
+				if !ok {
+					log.Printf("declining %s request...", req.Type)
+				}
+				req.Reply(ok, nil)
+			}
+		}(requests)
+	}
+}
+
+// =======================
+
+// parseDims extracts terminal dimensions (width x height) from the provided buffer.
+func parseDims(b []byte) (uint32, uint32) {
+	w := binary.BigEndian.Uint32(b)
+	h := binary.BigEndian.Uint32(b[4:])
+	return w, h
+}
+
+// ======================
+
+// Winsize stores the Height and Width of a terminal.
+type Winsize struct {
+	Height uint16
+	Width  uint16
+	x      uint16 // unused
+	y      uint16 // unused
+}
+
+// SetWinsize sets the size of the given pty.
+func SetWinsize(fd uintptr, w, h uint32) {
+	log.Printf("window resize %dx%d", w, h)
+	ws := &Winsize{Width: uint16(w), Height: uint16(h)}
+	syscall.Syscall(syscall.SYS_IOCTL, fd, uintptr(syscall.TIOCSWINSZ), uintptr(unsafe.Pointer(ws)))
+}
+
+// Borrowed from https://github.com/creack/termios/blob/master/win/win.go
